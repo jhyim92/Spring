c9751bd6c5a8640528b88e772bdcbd9f94f0613d
jhyim92 <jhyim92@naver.com>
2021-07-12 18:16:33 +0900

    Spring DI

---
+++ och03_DI/src/main/java/DI01/MainClass.java
@@ -0,0 +1,30 @@
+package DI01;
+
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+//		MyCalculator myCalculator = new MyCalculator();
+//		myCalculator.setCalculator(new Calculator());
+//		myCalculator.setFirstNum(10);
+//		myCalculator.setSecondNum(2);
+//
+//		myCalculator.add();
+//		myCalculator.sub();
+//		myCalculator.mul();
+//		myCalculator.div();
+
+//		DI 구현
+		String configLocation = "classpath:applicationCTX.xml";
+		AbstractApplicationContext ctx = new GenericXmlApplicationContext(configLocation);
+		MyCalculator myCalculator = ctx.getBean("myCalculator", MyCalculator.class);
+		myCalculator.add();
+		myCalculator.sub();
+		myCalculator.mul();
+		myCalculator.div();
+
+	}
+
+}
